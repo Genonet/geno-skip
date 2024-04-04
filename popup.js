ddce6c0a771f81032ccaf96684b91435d190c127
otokitoki <kaitoki03081122@gmail.com>
2024-04-05 01:56:22 +0900

    ボタンやめた 広告が流れたら発火させてみた

--- popup.js
+++ popup.js
@@ -1,17 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var controlButton = document.getElementById('controlButton');
-    controlButton.addEventListener('click', function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: () => {
-                    var videos = document.querySelectorAll('video');
-                    videos.forEach(function(video) {
-                        video.playbackRate = 16;
-                        video.muted = true;
-                    });
-                }
-            });
+// 監視ターゲットの取得
+const moviePlayer = document.getElementById('movie_player')
+
+// オブザーバーの作成
+const observer = new MutationObserver(records => {
+    // 広告が表示された場合
+    if (records[0].target.classList.contains("ad-showing")) {
+        var videos = document.querySelectorAll('video');
+        videos.forEach(function (video) {
+            video.playbackRate = 16;
+            video.muted = true;
         });
-    });
+    }
+});
+
+// 監視の開始
+observer.observe(moviePlayer, {
+    attributes: true,
+    attributeFilter: ['class']
 });
